e4f2b7da9878fc8e1664e101f73bc665c1bd34f9
fmedisov <fmedisov@gmail.com>
2021-02-13 23:09:05 +0300

    implementation

---
+++ src/test/java/microservices/example/backend/RequestsCounterControllerTests.java
@@ -0,0 +1,26 @@
+package microservices.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilderSupport;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(RequestsCounterController.class)
+public class RequestsCounterControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void firstRequest_one() throws Exception {
+        mockMvc.perform(get("/requests"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("1"));
+    }
+}
